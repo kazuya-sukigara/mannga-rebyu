bcaa821ea1bc4ec34e91c66d3c6e6ce2a87c7267
kazuya-sukigara <kazuyahahaha@icloud.com>
2020-06-21 14:14:55 +0000

    ジャンル機能修正

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,12 @@
 class GenresController < ApplicationController
 	def show
 		@genre = Genre.find(params[:id])
+
+		if @genre.ancestry == nil #@genreがancestry.nilがチェックする
+		   @children = Genre.where(ancestry: @genre.id)#nillの場合@genre.idをancestryに持つgenreをとる
+		   @manngas = Mannga.where(genre_id: @children.ids)#そのジャンルを持つmanngasをとる
+		else
 		@manngas = @genre.manngas
+	    end
 	end
 end
