40d2cd5df0fd79ea6252df7e17e3f61fc6fbf1b6
kazuya-sukigara <kazuyahahaha@icloud.com>
2020-06-25 15:47:20 +0000

    レイアウト修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def top
+		@manngas = Mannga.find(Favorite.group(:mannga_id).order('count(mannga_id) desc').limit(3).pluck(:mannga_id))
 		render layout:false
 	end
 
